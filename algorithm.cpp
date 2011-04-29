9ba4786d61e09929c6dcbf8a55c73a225c3ec10d
RiSK (sscrisk) <sscrisk@gmail.com>
2011-04-29 18:08:58 +0900

    Delete Demangle class

--- algorithm.cpp
+++ algorithm.cpp
@@ -9,36 +9,6 @@
 #include<iostream>
 #include<sscrisk/cel/algorithm.hpp>
 
-// for name demangling.
-#include<cstdlib>
-#include <cxxabi.h>
-class Demangle
-{
-private :
-    char * realname ;
-
-public :
-    Demangle( std::type_info const & ti )
-    {
-        int status = 0 ;
-        realname = abi::__cxa_demangle( ti.name(), 0, 0, &status ) ;
-    }
-
-    Demangle( Demangle const & ) = delete ;
-    Demangle & operator = ( Demangle const & ) = delete ;
-
-    ~Demangle()
-    {
-        std::free( realname ) ;
-    }
-
-    operator char const * () const
-    {
-        return realname ;
-    }
-
-} ;
-
 constexpr bool is_less_than_2(int n)
 {
  return n < 2;
