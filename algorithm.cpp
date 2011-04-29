fdfd7581fe0af9720ac5591964c0ba6200d58698
RiSK (sscrisk) <sscrisk@gmail.com>
2011-04-29 16:57:52 +0900

    Make namespace range and move range version functions to there

--- algorithm.cpp
+++ algorithm.cpp
@@ -33,213 +33,252 @@ public :
 
 } ;
 
-constexpr bool is_less_than_3(int n)
+constexpr bool is_less_than_2(int n)
 {
- return n < 3;
+ return n < 2;
 }
 
-constexpr bool is_greater_than_2(int n)
+constexpr bool is_greater_than_1(int n)
 {
- return n > 2;
+ return n > 1;
 }
 
-constexpr bool are_greater_than_3(int n, int m)
+constexpr bool are_greater_than_2(int n, int m)
 {
- return n > 3 && m > 3;
+ return n > 2 && m > 2;
 }
 
 int main()
 {
- using namespace std;
- using namespace sscrisk::cel;
-
  static constexpr int a[] = {0};
  static constexpr int b[] = {0, 1};
  static constexpr int c[] = {0, 1, 2};
  static constexpr int d[] = {0, 1, 2, 3};
+ static constexpr int e[] = {0, 1, 2, 3, 3, 4, 5, 0};
 
- // begin and end
  {
-  constexpr auto beg = begin(a);
-  constexpr auto ed = end(a);
-  assert(ed - beg == 1);
- }
+  using namespace sscrisk::cel;
 
- // all_of
- {
-  // empty
-  constexpr bool test = all_of(a, a, is_less_than_3);
-  assert(test);
-  // not empty
-  constexpr bool testa = all_of(a, is_less_than_3);
-  assert(testa);
-  constexpr bool testb = all_of(b, is_less_than_3);
-  assert(testb);
-  constexpr bool testc = all_of(c, is_less_than_3);
-  assert(testc);
-  constexpr bool testd = all_of(d, is_less_than_3);
-  assert(!testd);
- }
-
- // any_of
- {
-  // empty
-  constexpr bool test = any_of(a, a, is_greater_than_2);
-  assert(!test);
-  // not empty
-  constexpr bool testa = any_of(a, is_greater_than_2);
-  assert(!testa);
-  constexpr bool testb = any_of(b, is_greater_than_2);
-  assert(!testb);
-  constexpr bool testc = any_of(c, is_greater_than_2);
-  assert(!testc);
-  constexpr bool testd = any_of(d, is_greater_than_2);
-  assert(testd);
- }
-
- // none_of
- {
-  // empty
-  constexpr bool test = none_of(a, a, is_greater_than_2);
-  assert(test);
-  // not empty
-  constexpr bool testa = none_of(a, is_greater_than_2);
-  assert(testa);
-  constexpr bool testb = none_of(b, is_greater_than_2);
-  assert(testb);
-  constexpr bool testc = none_of(c, is_greater_than_2);
-  assert(testc);
-  constexpr bool testd = none_of(d, is_greater_than_2);
-  assert(!testd);
- }
-
- // find
- {
-  // empty
-  constexpr int const * test = find(d, d, 0);
-  assert(test == d);
-  // not empty
-  constexpr int const * test1 = find(d, end(d), 0);
-  assert(*test1 == 0);
-  constexpr int const * test2 = find(d, end(d), 3);
-  assert(*test2 == 3);
-  constexpr int const * test3 = find(d, end(d), 100);
-  assert(test3 == end(d));
-  constexpr auto test4 = find(d, 3);
-  constexpr auto test5 = test4.end();
-  constexpr auto test6 = test4.begin();
-  constexpr auto test7 = test5 - test6;
-  assert(test7 == 1);
- }
-
- // find_if
- {
-  constexpr auto iter = find_if(a, a, is_greater_than_2);
-  assert(iter == a);
+  // all_of
   {
-   constexpr auto range = find_if(a, is_greater_than_2);
-   constexpr auto first = range.begin(), last = range.end();
-   constexpr auto diff = last - first;
-   assert(diff == 0);
+   constexpr bool test1 = all_of(a, a, is_less_than_2);
+   assert(test1);
+   constexpr bool test2 = all_of(a, a + 1, is_less_than_2);
+   assert(test2);
+   constexpr bool test3 = all_of(c, c + 3, is_less_than_2);
+   assert(!test3);
   }
+
+  // any_of
   {
-   constexpr auto range = find_if(b, is_greater_than_2);
-   constexpr auto first = range.begin(), last = range.end();
-   constexpr auto diff = last - first;
-   assert(diff == 0);
+   constexpr bool test = any_of(a, a, is_greater_than_1);
+   assert(!test);
   }
+
+  // none_of
   {
-   constexpr auto range = find_if(c, is_greater_than_2);
-   constexpr auto first = range.begin(), last = range.end();
-   constexpr auto diff = last - first;
-   assert(diff == 0);
+   constexpr bool test = none_of(a, a, is_greater_than_1);
+   assert(test);
   }
+
+  // find
   {
-   constexpr auto range = find_if(d, is_greater_than_2);
-   constexpr auto first = range.begin(), last = range.end();
-   constexpr auto diff = last - first;
-   assert(diff == 1);
-   assert(*first > 2);
+   constexpr int const * test = find(d, d, 0);
+   assert(test == d);
+  }
+
+  // find_if
+  {
+   constexpr auto iter = find_if(a, a, is_greater_than_1);
+   assert(iter == a);
+  }
+
+  // find_if_not
+  {
+   constexpr auto iter = find_if_not(a, a, is_less_than_2);
+   assert(iter == a);
+  }
+
+  // adjacent_find
+  {
+   constexpr auto test1 = adjacent_find(a, a);
+   assert(test1 == a);
+   constexpr auto test2 = adjacent_find(a, a + 1);
+   assert(test2 == a + 1);
+   constexpr auto test3 = adjacent_find(b, b + 2);
+   assert(test3 == b + 2);
+   constexpr auto test4 = adjacent_find(e, e + 8);
+   assert(*test4 == 3);
+   assert(*(test4 + 1) == 3);
+   assert(test4 - e == 3);
+   constexpr auto test5 = adjacent_find(e, e + 8, are_greater_than_2);
+   assert(*test5 > 2);
+   assert(*(test5 + 1) > 2);
+   assert(test5 - e == 3);
+  }
+
+  // count
+  {
+   constexpr auto test1 = count(a, a, 0);
+   assert(test1 == 0);
+  }
+
+  // count_if
+  {
+   constexpr auto test1 = count_if(a, a, is_less_than_2);
+   assert(test1 == 0);
   }
  }
 
- // find_if_not
+ // range
  {
-  constexpr auto iter = find_if_not(a, a, is_less_than_3);
-  assert(iter == a);
-  {
-   constexpr auto range = find_if_not(a, is_less_than_3);
-   constexpr auto first = range.begin(), last = range.end();
-   constexpr auto diff = last - first;
-   assert(diff == 0);
-  }
-  {
-   constexpr auto range = find_if_not(b, is_less_than_3);
-   constexpr auto first = range.begin(), last = range.end();
-   constexpr auto diff = last - first;
-   assert(diff == 0);
-  }
-  {
-   constexpr auto range = find_if_not(c, is_less_than_3);
-   constexpr auto first = range.begin(), last = range.end();
-   constexpr auto diff = last - first;
-   assert(diff == 0);
-  }
-  {
-   constexpr auto range = find_if_not(d, is_less_than_3);
-   constexpr auto first = range.begin(), last = range.end();
-   constexpr auto diff = last - first;
-   assert(diff == 1);
-   assert(!(*first < 3));
-  }
- }
+  using namespace sscrisk::cel::range;
 
- // adjacent_find
- {
-  constexpr auto test1 = adjacent_find(a, a);
-  assert(test1 == a);
-  constexpr auto test2 = adjacent_find(begin(a), end(a));
-  assert(test2 == end(a));
-  constexpr auto test3 = adjacent_find(begin(b), end(b));
-  assert(test3 == end(b));
-  static constexpr int e[] = {0, 1, 2, 3, 3, 4, 5};
-  constexpr auto test4 = adjacent_find(begin(e), end(e));
-  assert(*test4 == 3);
-  assert(*(test4 + 1) == 3);
-  assert(test4 - e == 3);
-  constexpr auto test5 = adjacent_find(begin(e), end(e), are_greater_than_3);
-  assert(*test5 > 3);
-  assert(*(test5 + 1) > 3);
-  assert(test5 - e == 5);
- }
+  // begin and end
+  {
+   constexpr auto beg = begin(a);
+   constexpr auto ed = end(a);
+   assert(ed - beg == 1);
+  }
 
- // count
- {
-  constexpr auto test1 = count(a, a, 0);
-  assert(test1 == 0);
-  constexpr auto test2 = count(a, 0);
-  assert(test2 == 1);
-  constexpr auto test3 = count(d, 3);
-  assert(test3 == 1);
-  static constexpr int e[] = {0, 1, 2, 0, 1, 2, 0};
-  constexpr auto test4 = count(e, 0);
-  assert(test4 == 3);
-  constexpr auto test5 = count(e, 3);
-  assert(test5 == 0);
- }
+  // all_of
+  {
+   constexpr bool testa = all_of(a, is_less_than_2);
+   assert(testa);
+   constexpr bool testb = all_of(b, is_less_than_2);
+   assert(testb);
+   constexpr bool testc = all_of(c, is_less_than_2);
+   assert(!testc);
+   constexpr bool testd = all_of(d, is_less_than_2);
+   assert(!testd);
+  }
 
- // count_if
- {
-  constexpr auto test1 = count_if(a, a, is_less_than_3);
-  assert(test1 == 0);
-  constexpr auto test2 = count_if(a, is_less_than_3);
-  assert(test2 == 1);
-  constexpr auto test3 = count_if(d, is_less_than_3);
-  assert(test3 == 3);
-  static constexpr int e[] = {3, 1, 2, 3, 1, 2, 3};
-  constexpr auto test4 = count_if(e, is_less_than_3);
-  assert(test4 == 4);
-  constexpr auto test5 = count_if(e, is_greater_than_2);
-  assert(test5 == 3);
+  // any_of
+  {
+   constexpr bool testa = any_of(a, is_greater_than_1);
+   assert(!testa);
+   constexpr bool testb = any_of(b, is_greater_than_1);
+   assert(!testb);
+   constexpr bool testc = any_of(c, is_greater_than_1);
+   assert(testc);
+   constexpr bool testd = any_of(d, is_greater_than_1);
+   assert(testd);
+  }
+
+  // none_of
+  {
+   constexpr bool testa = none_of(a, is_greater_than_1);
+   assert(testa);
+   constexpr bool testb = none_of(b, is_greater_than_1);
+   assert(testb);
+   constexpr bool testc = none_of(c, is_greater_than_1);
+   assert(!testc);
+   constexpr bool testd = none_of(d, is_greater_than_1);
+   assert(!testd);
+  }
+
+  // find
+  {
+   constexpr auto test1 = find(d, 0);
+   assert(*test1.begin() == 0);
+   constexpr auto test2 = find(d, 3);
+   assert(*test2.begin() == 3);
+   constexpr auto test3 = find(d, 100);
+   assert(test3.end() == end(d));
+   constexpr auto test4 = find(d, 3);
+   constexpr auto test5 = test4.end();
+   constexpr auto test6 = test4.begin();
+   constexpr auto test7 = test5 - test6;
+   assert(test7 == 1);
+  }
+
+  // find_if
+  {
+   {
+    constexpr auto range = find_if(a, is_greater_than_1);
+    constexpr auto first = range.begin(), last = range.end();
+    constexpr auto diff = last - first;
+    assert(diff == 0);
+   }
+   {
+    constexpr auto range = find_if(b, is_greater_than_1);
+    constexpr auto first = range.begin(), last = range.end();
+    constexpr auto diff = last - first;
+    assert(diff == 0);
+   }
+   {
+    constexpr auto range = find_if(c, is_greater_than_1);
+    constexpr auto first = range.begin(), last = range.end();
+    constexpr auto diff = last - first;
+    assert(diff == 1);
+    assert(*first > 1);
+   }
+   {
+    constexpr auto range = find_if(d, is_greater_than_1);
+    constexpr auto first = range.begin(), last = range.end();
+    constexpr auto diff = last - first;
+    assert(diff == 2);
+    assert(*first > 1);
+   }
+  }
+
+  // find_if_not
+  {
+   {
+    constexpr auto range = find_if_not(a, is_less_than_2);
+    constexpr auto first = range.begin(), last = range.end();
+    constexpr auto diff = last - first;
+    assert(diff == 0);
+   }
+   {
+    constexpr auto range = find_if_not(b, is_less_than_2);
+    constexpr auto first = range.begin(), last = range.end();
+    constexpr auto diff = last - first;
+    assert(diff == 0);
+   }
+   {
+    constexpr auto range = find_if_not(c, is_less_than_2);
+    constexpr auto first = range.begin(), last = range.end();
+    constexpr auto diff = last - first;
+    assert(diff == 1);
+    assert(!(*first < 2));
+   }
+   {
+    constexpr auto range = find_if_not(d, is_less_than_2);
+    constexpr auto first = range.begin(), last = range.end();
+    constexpr auto diff = last - first;
+    assert(diff == 2);
+    assert(!(*first < 2));
+   }
+  }
+
+  // adjacent_find
+  {
+  }
+
+  // count
+  {
+   constexpr auto test2 = count(a, 0);
+   assert(test2 == 1);
+   constexpr auto test3 = count(d, 3);
+   assert(test3 == 1);
+   static constexpr int e[] = {0, 1, 2, 0, 1, 2, 0};
+   constexpr auto test4 = count(e, 0);
+   assert(test4 == 3);
+   constexpr auto test5 = count(e, 3);
+   assert(test5 == 0);
+  }
+
+  // count_if
+  {
+   constexpr auto test2 = count_if(a, is_less_than_2);
+   assert(test2 == 1);
+   constexpr auto test3 = count_if(d, is_less_than_2);
+   assert(test3 == 2);
+   constexpr auto test4 = count_if(e, is_less_than_2);
+   assert(test4 == 3);
+   constexpr auto test5 = count_if(e, is_greater_than_1);
+   assert(test5 == 5);
+  }
  }
 }
--- algorithm.hpp
+++ algorithm.hpp
@@ -12,56 +12,11 @@
 
 namespace sscrisk{ namespace cel{
 
-  template<class Range>
-  constexpr typename Range::const_iterator begin(Range const & range)
-  {
-   return range.begin();
-  }
-  
-  template<class T, size_t N>
-  constexpr T const * begin(T const (& array)[N])
-  {
-   return array;
-  }
-
-  template<class Range>
-  constexpr typename Range::const_iterator end(Range const & range)
-  {
-   return range.end();
-  }
-  
-  template<class T, size_t N>
-  constexpr T const * end(T const (& array)[N])
-  {
-   return array + N;
-  }
-
-  template<class Iterator>
-  struct range_container
-  {
-   typedef Iterator const_iterator;
-   typedef typename std::iterator_traits<Iterator>::difference_type difference_type;
-  private:
-   Iterator first, last;
-  public:
-   constexpr range_container(Iterator first, Iterator last)
-    : first(first), last(last)
-    {}
-   constexpr Iterator begin()const{ return first; }
-   constexpr Iterator end()const{ return last; }
-  };
-
   // 25.2.1 All of
   template<class Iterator, class Predicate>
   constexpr bool all_of(Iterator first, Iterator last, Predicate pred)
   {
-   return first == last ? true : pred(*first) && all_of(first + 1, last, pred);
-  }
-
-  template<class Range, class Predicate>
-  constexpr bool all_of(Range const & range, Predicate pred)
-  {
-   return all_of(sscrisk::cel::begin(range), sscrisk::cel::end(range), pred);
+   return first == last ? true : pred(*first) == true && all_of(first + 1, last, pred);
   }
 
   // 25.2.2 Any of
@@ -71,23 +26,11 @@ namespace sscrisk{ namespace cel{
    return first == last ? false : pred(*first) || any_of(first + 1, last, pred);
   }
 
-  template<class Range, class Predicate>
-  constexpr bool any_of(Range const & range, Predicate pred)
-  {
-   return any_of(begin(range), end(range), pred);
-  }
-
   // 25.2.3 None of
   template <class InputIterator, class Predicate>
   constexpr bool none_of(InputIterator first, InputIterator last, Predicate pred)
   {
    return first == last ? true : !pred(*first) && none_of(first + 1, last, pred);
-  }
-
-  template<class Range, class Predicate>
-  constexpr bool none_of(Range const & range, Predicate pred)
-  {
-   return none_of(begin(range), end(range), pred);
   }
 
   // 25.2.5 Find
@@ -97,51 +40,16 @@ namespace sscrisk{ namespace cel{
    return first == last ? last : *first == value ? first : find(first + 1, last, value);
   }
 
-  template<class Array>
-  struct array_to_const_ptr
-  {
-   typedef typename std::decay<
-    typename std::add_const<Array>::type
-   >::type type;
-  };
-  
-  template<class Range, class T>
-  constexpr range_container<
-   typename array_to_const_ptr<Range>::type
-  >
-  find(Range const & range, T const & value)
-  {
-   return {find(begin(range), end(range), value), end(range)};
-  }
-
   template<class InputIterator, class Predicate>
   constexpr InputIterator find_if(InputIterator first, InputIterator last, Predicate pred)
   {
    return first == last ? last : pred(*first) ? first : find_if(first + 1, last, pred);
   }
 
-  template<class Range, class Predicate>
-  constexpr range_container<
-   typename array_to_const_ptr<Range>::type
-  >
-  find_if(Range const & range, Predicate pred)
-  {
-   return {find_if(begin(range), end(range), pred), end(range)};
-  }
-  
   template<class InputIterator, class Predicate>
   constexpr InputIterator find_if_not(InputIterator first, InputIterator last, Predicate pred)
   {
    return first == last ? last : !pred(*first) ? first : find_if_not(first + 1, last, pred);
-  }
-
-  template<class Range, class Predicate>
-  constexpr range_container<
-   typename array_to_const_ptr<Range>::type
-  >
-  find_if_not(Range const & range, Predicate pred)
-  {
-   return {find_if_not(begin(range), end(range), pred), end(range)};
   }
 
   // 25.2.8 Adjacent find
@@ -167,13 +75,6 @@ namespace sscrisk{ namespace cel{
    return first == last ? 0 : (*first == value ? 1 : 0) + count(first + 1, last, value);
   }
 
-  template<class Range, class T>
-  constexpr typename std::iterator_traits<typename array_to_const_ptr<Range>::type>::difference_type
-  count(Range const & range, T const & value)
-  {
-   return count(begin(range), end(range), value);
-  }
-  
   template<class InputIterator, class Predicate>
   constexpr typename std::iterator_traits<InputIterator>::difference_type
   count_if(InputIterator first, InputIterator last, Predicate pred)
@@ -181,11 +82,121 @@ namespace sscrisk{ namespace cel{
    return first == last ? 0 : (pred(*first) ? 1 : 0) + count_if(first + 1, last, pred);
   }
 
-  template<class Range, class Predicate>
-  constexpr typename std::iterator_traits<typename array_to_const_ptr<Range>::type>::difference_type
-  count_if(Range const & range, Predicate pred)
-  {
-   return count_if(begin(range), end(range), pred);
+  namespace range{
+
+   namespace cel = ::sscrisk::cel;
+
+   template<class Range>
+   constexpr typename Range::const_iterator begin(Range const & range)
+   {
+    return range.begin();
+   }
+  
+   template<class T, size_t N>
+   constexpr T const * begin(T const (& array)[N])
+   {
+    return array;
+   }
+
+   template<class Range>
+   constexpr typename Range::const_iterator end(Range const & range)
+   {
+    return range.end();
+   }
+  
+   template<class T, size_t N>
+   constexpr T const * end(T const (& array)[N])
+   {
+    return array + N;
+   }
+
+   template<class Array>
+   struct array_to_const_ptr
+   {
+    typedef typename std::decay<
+     typename std::add_const<Array>::type
+    >::type type;
+   };
+
+   template<class Iterator>
+   struct range_container
+   {
+    typedef Iterator const_iterator;
+    typedef typename std::iterator_traits<Iterator>::difference_type difference_type;
+   private:
+    Iterator first, last;
+   public:
+    constexpr range_container(Iterator first, Iterator last)
+     : first(first), last(last)
+     {}
+    constexpr Iterator begin()const{ return first; }
+    constexpr Iterator end()const{ return last; }
+   };
+
+   // 25.2.1 All of
+   template<class Range, class Predicate>
+   constexpr bool all_of(Range const & range, Predicate pred)
+   {
+    return cel::all_of(begin(range), end(range), pred);
+   }
+
+   // 25.2.2 Any of
+   template<class Range, class Predicate>
+   constexpr bool any_of(Range const & range, Predicate pred)
+   {
+    return cel::any_of(begin(range), end(range), pred);
+   }
+
+   // 25.2.3 None of
+   template<class Range, class Predicate>
+   constexpr bool none_of(Range const & range, Predicate pred)
+   {
+    return cel::none_of(begin(range), end(range), pred);
+   }
+
+   // 25.2.5 Find
+   template<class Range, class T>
+   constexpr range_container<
+    typename array_to_const_ptr<Range>::type
+   >
+   find(Range const & range, T const & value)
+   {
+    return {cel::find(begin(range), end(range), value), end(range)};
+   }
+
+   template<class Range, class Predicate>
+   constexpr range_container<
+    typename array_to_const_ptr<Range>::type
+   >
+   find_if(Range const & range, Predicate pred)
+   {
+    return {cel::find_if(begin(range), end(range), pred), end(range)};
+   }
+  
+   template<class Range, class Predicate>
+   constexpr range_container<
+    typename array_to_const_ptr<Range>::type
+   >
+   find_if_not(Range const & range, Predicate pred)
+   {
+    return {cel::find_if_not(begin(range), end(range), pred), end(range)};
+   }
+
+   // 25.2.9 Count
+   template<class Range, class T>
+   constexpr typename std::iterator_traits<typename array_to_const_ptr<Range>::type>::difference_type
+   count(Range const & range, T const & value)
+   {
+    return cel::count(begin(range), end(range), value);
+   }
+
+   template<class Range, class Predicate>
+   constexpr typename std::iterator_traits<typename array_to_const_ptr<Range>::type>::difference_type
+   count_if(Range const & range, Predicate pred)
+   {
+    return cel::count_if(begin(range), end(range), pred);
+   }
+
   }
 
 }}
