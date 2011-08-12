d00c24269270ee7a956ad62e8ecd3198f111a11a
RiSK (sscrisk) <sscrisk@gmail.com>
2011-08-12 23:54:31 +0900

    Add files cctype.hpp, cctype.cpp and ascii.inl

---
+++ cctype.cpp
@@ -0,0 +1,240 @@
+// Copyright (C) 2011 RiSK (sscrisk)
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// コンパイルおよび実行環境の文字が ASCII 互換であることを前提としたテストをしている。
+// ASCII のテストなのだから、本来は 'A' ではなく、0x41 を使うべき。
+
+#include<cassert>
+#include<sscrisk/cel/cctype.hpp>
+
+int main()
+{
+ namespace c = sscrisk::cel;
+ {
+  constexpr bool test1 = c::isalnum('A');
+  assert(test1 == true);
+  constexpr bool test2 = c::isalnum('a');
+  assert(test2 == true);
+  constexpr bool test3 = c::isalnum('0');
+  assert(test3 == true);
+  constexpr bool test4 = c::isalnum(' ');
+  assert(test4 == false);
+  constexpr bool test5 = c::isalnum('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::isalnum('f');
+  assert(test6 == true);
+  constexpr bool test7 = c::isalnum(',');
+  assert(test7 == false);
+ }
+ {
+  constexpr bool test1 = c::isalpha('A');
+  assert(test1 == true);
+  constexpr bool test2 = c::isalpha('a');
+  assert(test2 == true);
+  constexpr bool test3 = c::isalpha('0');
+  assert(test3 == false);
+  constexpr bool test4 = c::isalpha(' ');
+  assert(test4 == false);
+  constexpr bool test5 = c::isalpha('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::isalpha('f');
+  assert(test6 == true);
+  constexpr bool test7= c::isalpha(',');
+  assert(test7 == false);
+ }
+ {
+  constexpr bool test1 = c::isblank('A');
+  assert(test1 == false);
+  constexpr bool test2 = c::isblank('a');
+  assert(test2 == false);
+  constexpr bool test3 = c::isblank('0');
+  assert(test3 == false);
+  constexpr bool test4 = c::isblank(' ');
+  assert(test4 == true);
+  constexpr bool test5 = c::isblank('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::isblank('f');
+  assert(test6 == false);
+  constexpr bool test7 = c::isblank(',');
+  assert(test7 == false);
+ }
+ {
+  constexpr bool test1 = c::iscntrl('A');
+  assert(test1 == false);
+  constexpr bool test2 = c::iscntrl('a');
+  assert(test2 == false);
+  constexpr bool test3 = c::iscntrl('0');
+  assert(test3 == false);
+  constexpr bool test4 = c::iscntrl(' ');
+  assert(test4 == false);
+  constexpr bool test5 = c::iscntrl('\a');
+  assert(test5 == true);
+  constexpr bool test6 = c::iscntrl('f');
+  assert(test6 == false);
+  constexpr bool test7 = c::iscntrl(',');
+  assert(test7 == false);
+ }
+ {
+  constexpr bool test1 = c::isdigit('A');
+  assert(test1 == false);
+  constexpr bool test2 = c::isdigit('a');
+  assert(test2 == false);
+  constexpr bool test3 = c::isdigit('0');
+  assert(test3 == true);
+  constexpr bool test4 = c::isdigit(' ');
+  assert(test4 == false);
+  constexpr bool test5 = c::isdigit('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::isdigit('f');
+  assert(test6 == false);
+  constexpr bool test7 = c::isdigit(',');
+  assert(test7 == false);
+ }
+ {
+  constexpr bool test1 = c::isgraph('A');
+  assert(test1 == true);
+  constexpr bool test2 = c::isgraph('a');
+  assert(test2 == true);
+  constexpr bool test3 = c::isgraph('0');
+  assert(test3 == true);
+  constexpr bool test4 = c::isgraph(' ');
+  assert(test4 == false);
+  constexpr bool test5 = c::isgraph('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::isgraph('f');
+  assert(test6 == true);
+  constexpr bool test7 = c::isgraph(',');
+  assert(test7 == true);
+ }
+ {
+  constexpr bool test1 = c::islower('A');
+  assert(test1 == false);
+  constexpr bool test2 = c::islower('a');
+  assert(test2 == true);
+  constexpr bool test3 = c::islower('0');
+  assert(test3 == false);
+  constexpr bool test4 = c::islower(' ');
+  assert(test4 == false);
+  constexpr bool test5 = c::islower('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::islower('f');
+  assert(test6 == true);
+  constexpr bool test7 = c::islower(',');
+  assert(test7 == false);
+ }
+ {
+  constexpr bool test1 = c::isprint('A');
+  assert(test1 == true);
+  constexpr bool test2 = c::isprint('a');
+  assert(test2 == true);
+  constexpr bool test3 = c::isprint('0');
+  assert(test3 == true);
+  constexpr bool test4 = c::isprint(' ');
+  assert(test4 == true);
+  constexpr bool test5 = c::isprint('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::isprint('f');
+  assert(test6 == true);
+  constexpr bool test7 = c::isprint(',');
+  assert(test7 == true);
+ }
+ {
+  constexpr bool test1 = c::ispunct('A');
+  assert(test1 == false);
+  constexpr bool test2 = c::ispunct('a');
+  assert(test2 == false);
+  constexpr bool test3 = c::ispunct('0');
+  assert(test3 == false);
+  constexpr bool test4 = c::ispunct(' ');
+  assert(test4 == false);
+  constexpr bool test5 = c::ispunct('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::ispunct('f');
+  assert(test6 == false);
+  constexpr bool test7 = c::ispunct(',');
+  assert(test7 == true);
+ }
+ {
+  constexpr bool test1 = c::isspace('A');
+  assert(test1 == false);
+  constexpr bool test2 = c::isspace('a');
+  assert(test2 == false);
+  constexpr bool test3 = c::isspace('0');
+  assert(test3 == false);
+  constexpr bool test4 = c::isspace(' ');
+  assert(test4 == true);
+  constexpr bool test5 = c::isspace('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::isspace('f');
+  assert(test6 == false);
+  constexpr bool test7 = c::isspace(',');
+  assert(test7 == false);
+ }
+ {
+  constexpr bool test1 = c::isupper('A');
+  assert(test1 == true);
+  constexpr bool test2 = c::isupper('a');
+  assert(test2 == false);
+  constexpr bool test3 = c::isupper('0');
+  assert(test3 == false);
+  constexpr bool test4 = c::isupper(' ');
+  assert(test4 == false);
+  constexpr bool test5 = c::isupper('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::isupper('f');
+  assert(test6 == false);
+  constexpr bool test7 = c::isupper(',');
+  assert(test7 == false);
+ }
+ {
+  constexpr bool test1 = c::isxdigit('A');
+  assert(test1 == true);
+  constexpr bool test2 = c::isxdigit('a');
+  assert(test2 == true);
+  constexpr bool test3 = c::isxdigit('0');
+  assert(test3 == true);
+  constexpr bool test4 = c::isxdigit(' ');
+  assert(test4 == false);
+  constexpr bool test5 = c::isxdigit('\a');
+  assert(test5 == false);
+  constexpr bool test6 = c::isxdigit('f');
+  assert(test6 == true);
+  constexpr bool test7 = c::isxdigit(',');
+  assert(test7 == false);
+ }
+ {
+  constexpr char test1 = c::tolower('A');
+  assert(test1 == 'a');
+  constexpr char test2 = c::tolower('a');
+  assert(test2 == 'a');
+  constexpr char test3 = c::tolower('0');
+  assert(test3 == '0');
+  constexpr char test4 = c::tolower(' ');
+  assert(test4 == ' ');
+  constexpr char test5 = c::tolower('\a');
+  assert(test5 == '\a');
+  constexpr char test6 = c::tolower('f');
+  assert(test6 == 'f');
+  constexpr char test7 = c::tolower(',');
+  assert(test7 == ',');
+ }
+ {
+  constexpr char test1 = c::toupper('A');
+  assert(test1 == 'A');
+  constexpr char test2 = c::toupper('a');
+  assert(test2 == 'A');
+  constexpr char test3 = c::toupper('0');
+  assert(test3 == '0');
+  constexpr char test4 = c::toupper(' ');
+  assert(test4 == ' ');
+  constexpr char test5 = c::toupper('\a');
+  assert(test5 == '\a');
+  constexpr char test6 = c::toupper('f');
+  assert(test6 == 'F');
+  constexpr char test7 = c::toupper(',');
+  assert(test7 == ',');
+ }
+}
---
+++ cctype.hpp
@@ -0,0 +1,129 @@
+// Copyright (C) 2011 RiSK (sscrisk)
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(SSCRISK_CEL_CCTYPE_HPP)
+#define SSCRISK_CEL_CCTYPE_HPP
+#if defined(_MSC_VER) && _MSC_VER >= 1020
+#pragma once
+#endif
+
+// cctype.hpp
+
+// ref: ISO/IEC 9899:1999
+
+// Supported only ASCII.
+
+namespace sscrisk{ namespace cel{
+
+namespace detail{
+
+constexpr unsigned alpha = 1 << 0;
+constexpr unsigned blank = 1 << 1;
+constexpr unsigned cntrl = 1 << 2;
+constexpr unsigned digit = 1 << 3;
+constexpr unsigned graph = 1 << 4;
+constexpr unsigned lower = 1 << 5;
+constexpr unsigned print = 1 << 6;
+constexpr unsigned punct = 1 << 7;
+constexpr unsigned space = 1 << 8;
+constexpr unsigned upper = 1 << 9;
+constexpr unsigned xdigit = 1 << 10;
+constexpr unsigned table[] = {
+#include<sscrisk/cel/ascii.inl>
+};
+
+}
+
+constexpr int isalpha(int);
+constexpr int isdigit(int);
+
+// 7.4.1.1 The isalnum function
+constexpr int isalnum(int c)
+{
+ return isalpha(c) || isdigit(c);
+}
+
+// 7.4.1.2 The isalpha function
+constexpr int isalpha(int c)
+{
+ return detail::table[c] & detail::alpha;
+}
+
+// 7.4.1.3 The isblank function
+constexpr int isblank(int c)
+{
+ return detail::table[c] & detail::blank;
+}
+
+// 7.4.1.4 The iscntrl function
+constexpr int iscntrl(int c)
+{
+ return detail::table[c] & detail::cntrl;
+}
+
+// 7.4.1.5 The isdigit function
+constexpr int isdigit(int c)
+{
+ return detail::table[c] & detail::digit;
+}
+
+// 7.4.1.6 The isgraph function
+constexpr int isgraph(int c)
+{
+ return detail::table[c] & detail::graph;
+}
+
+// 7.4.1.7 The islower function
+constexpr int islower(int c)
+{
+ return detail::table[c] & detail::lower;
+}
+
+// 7.4.1.8 The isprint function
+constexpr int isprint(int c)
+{
+ return detail::table[c] & detail::print;
+}
+
+// 7.4.1.9 The ispunct function
+constexpr int ispunct(int c)
+{
+ return detail::table[c] & detail::punct;
+}
+
+// 7.4.1.10 The isspace function
+constexpr int isspace(int c)
+{
+ return detail::table[c] & detail::space;
+}
+
+// 7.4.1.11 The isupper function
+constexpr int isupper(int c)
+{
+ return detail::table[c] & detail::upper;
+}
+
+// 7.4.1.12 The isxdigit function
+constexpr int isxdigit(int c)
+{
+ return detail::table[c] & detail::xdigit;
+}
+
+// 7.4.2.1 The tolower function
+constexpr int tolower(int c)
+{
+ return isupper(c) ? c + (0x61/* 'a' */ - 0x41/* 'A' */) : c;
+}
+
+// 7.4.2.2 The toupper function
+constexpr int toupper(int c)
+{
+ return islower(c) ? c - (0x61/* 'a' */ - 0x41/* 'A' */) : c;
+}
+
+}}
+
+#endif
